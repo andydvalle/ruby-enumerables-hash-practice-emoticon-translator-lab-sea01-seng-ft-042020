a605e85b1cf790440f0fa3300b55616911f6e727
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-11 19:11:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,9 +27,7 @@ def get_english_meaning(yaml, english_element)
   
   uploaded_hash.each do |key, inner_hash|
   binding.pry
-    if true 
       message = "Sorry, that emoticon was not found"
       puts message
-    end
   end
 end

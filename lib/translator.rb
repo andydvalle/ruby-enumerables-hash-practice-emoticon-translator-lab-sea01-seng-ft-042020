5e77042984a2e4905070fd072bec2915932cae4f
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-11 18:41:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,7 +28,7 @@ def get_english_meaning(yaml, english_element)
     uploaded_hash.each do |emoticon_name, inner_hash|
     inner_hash.each do |english_japanese, english_element|
       if inner_hash[:english] == english_element
-        return inner_hash[:japanese]
+        return emoticon_name
       end
     end
   end
